6dd8ce662bcf41905461dafd3988a457e2a9436c
Emanuele Ballarin <emanuele@ballarin.cc>
2022-04-10 23:52:19 +0200

    Update uninstall.ps1

--- uninstall.ps1
+++ uninstall.ps1
@@ -2,6 +2,6 @@
 # Run as Administrator, from an Administrator PowerShell instance, or with `sudo` by `gerardog` and the like.
 #
 
-cd "C:\Program Files\Image-Line\FL Studio ASIO\"
+Set-Location "C:\Program Files\Image-Line\FL Studio ASIO\"
 unregisterAsioDriver.bat
 Remove-Item -R -Force "C:\Program Files\Image-Line\FL Studio ASIO\"
